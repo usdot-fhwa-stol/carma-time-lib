354ebd06c9287f6040fc98d4c64a1cf5bf39b144
dev <paul.k.bourelly@leidos.com>
2024-05-22 18:21:11 -0400

    Updates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,4 @@
 
 set -ex
 # Get ubuntu distribution code name. All STOL APT debian packages are pushed to S3 bucket based on distribution codename.
-apt install python3-dev && true
+apt install -y python3-dev && true
