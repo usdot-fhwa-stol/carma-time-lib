fe2f7aa2f6b4a167d332f0057be63abce032338a
dev <paul.k.bourelly@leidos.com>
2024-05-22 17:51:05 -0400

    Updates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,5 +2,5 @@
 
 set -ex
 # Get ubuntu distribution code name. All STOL APT debian packages are pushed to S3 bucket based on distribution codename.
-/opt/carma/scripts/install_dependencies.sh python3-dev 
+/opt/carma/scripts/install_dependencies_script.sh python3-dev 
 # TODO
