98a720267efee18734dd2844f35f3d155325df9c
dev <paul.k.bourelly@leidos.com>
2024-05-22 14:56:32 -0400

    Updates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -ex
+# Get ubuntu distribution code name. All STOL APT debian packages are pushed to S3 bucket based on distribution codename.
+source /etc/lsb-release
+echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${DISTRIB_CODENAME} main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${DISTRIB_CODENAME}-updates main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${DISTRIB_CODENAME}-security main restricted universe multiverse" >> /etc/apt/sources.list
 apt update
 apt install -y python3-dev 
 # TODO
